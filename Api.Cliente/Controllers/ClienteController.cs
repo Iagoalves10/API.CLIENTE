83a48af52f52c4d360cee284d01e5273ee6681af
Iago Alves <iago.alves@bit.com.br>
2022-03-24 14:02:27 -0300

    original

---
+++ Api.Cliente/Controllers/ClienteController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Api.Cliente.Models;
+using System.Data.SqlClient;
+using Api.Cliente.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Cliente.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class ClienteController : ControllerBase
+    {
+        Conexao conexao = new Conexao();
+        SqlCommand sqlCommand = new SqlCommand();
+        public string mensagem = "";
+         
+
+        [HttpPost]
+        [Route("/api/v1/clientes")]
+        public ActionResult<ClienteModel> AdicionarCliente([FromBody] ClienteModel cliente)
+        {
+            sqlCommand.CommandText = "INSERT INTO cliente (cpf, nome, sobrenome, datadenascimento)" +
+                 "VALUES (@cpf, @nome, @sobrenome, @datadenascimento )";
+
+            sqlCommand.Parameters.AddWithValue("@cpf", cliente.Cpf);
+            sqlCommand.Parameters.AddWithValue("@nome", cliente.Nome);
+            sqlCommand.Parameters.AddWithValue("@sobrenome", cliente.Sobrenome);
+            sqlCommand.Parameters.AddWithValue("@datadenascimento", cliente.DataDeNascimento);
+
+            try
+            {
+                sqlCommand.Connection = conexao.Conectar();
+                sqlCommand.ExecuteNonQuery();
+                conexao.desconectar();
+                this.mensagem = "Cliente Cadastrado!!";
+            }
+            catch (SqlException ex)
+            {
+                this.mensagem = "Erro ao cadastrar cliente";
+            }
+
+            return cliente;
+        }
+
+        [HttpDelete]
+        [Route("/api/v1/clientes/{cpf}")]
+        public ActionResult<ClienteModel> ExcluirCliente(string cpf)
+        {
+            object parametro = cpf;
+            sqlCommand.CommandText = "DELETE FROM cliente WHERE cpf = @cpf ;";
+            object parametrosSql = new { @cpf = cpf };
+
+            try
+            {
+                sqlCommand.Connection = conexao.Conectar();
+                sqlCommand.ExecuteNonQuery();
+                conexao.desconectar();
+                this.mensagem = "Cliente Cadastrado!!";
+            }
+            catch (SqlException ex)
+            {
+                this.mensagem = "Erro ao cadastrar cliente";
+            }
+
+            return Ok(cpf);
+         }
+    }
+}
+
+
---
+++ Api.Cliente/DataContext/ClienteContext.cs
@@ -0,0 +1,35 @@
+﻿
+using Api.Cliente.Models;
+using System.Data.SqlClient;
+
+namespace Api.Cliente.DataContext
+{
+    public class Conexao
+    {
+        SqlConnection sql = new SqlConnection();
+        public Conexao()
+        {
+            sql.ConnectionString = ("Server=localhost;Database=BitPagg; User Id=sa; Password=Senha@123456;");
+        }
+
+        public Task<IEnumerable<ClienteModel>> ClienteModel { get; internal set; }
+
+        public SqlConnection Conectar()
+        {
+            if (sql.State == System.Data.ConnectionState.Closed)
+            {
+                sql.Open();
+            }
+            return sql;
+        }
+
+        public void desconectar()
+        {
+            if (sql.State ==System.Data.ConnectionState.Open)
+            {
+                sql.Close();
+            }
+        }
+             
+    }
+}
